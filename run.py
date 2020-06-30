390973f0bff1002c0030c784924439105f8dafaa
Tuomo <tuokri@tuta.io>
2020-07-01 01:05:58 +0300

    Stylish Discord webhook embed

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import datetime
+import re
 import socket
 import sys
 import threading
@@ -5,6 +7,7 @@ from collections import defaultdict
 from configparser import ConfigParser
 from socketserver import BaseRequestHandler
 from socketserver import ThreadingTCPServer
+from typing import Optional
 from typing import Tuple
 
 import discord
@@ -20,6 +23,12 @@ StreamHandler(
 RotatingFileHandler(
     "tklserver.log", level="INFO", bubble=True).push_application()
 logger = Logger("tklserver")
+
+STEAM_PROFILE_URL = "https://www.steamcommunity.com/profiles/{id}"
+DATE_FMT = "%Y/%m/%d - %H:%M:%S"
+TKL_MSG_PAT = re.compile(
+    r"(\([0-9]{4}/[0-9]{2}/[0-9]{2}\s-\s[0-9]{2}:[0-9]{2}:[0-9]{2}\))\s'(.+)'\s"
+    r"\[(0x[0-9a-fA-F]+)\]\s(killed|teamkilled)\s'(.+)'\s\[(0x[0-9a-fA-F]+)\]\swith\s<(.+)>")
 
 
 class TKLServer(ThreadingTCPServer):
@@ -46,13 +55,88 @@ class TKLRequestHandler(BaseRequestHandler):
         super().__init__(request, client_address, server)
 
     def execute_webhook(self, ident: str, msg: str):
-        logger.info("sending webhook message for {i}", i=ident)
+        # TODO: embed kill weapon image.
+
+        embed: Optional[discord.Embed] = None
+        try:
+            msg_match = TKL_MSG_PAT.match(msg)
+            groups = msg_match.groups()
+            if not msg_match:
+                logger.warn("message does not match pattern")
+            else:
+                date = groups[0]
+                isodate = datetime.datetime.strptime(date, DATE_FMT).isoformat()
+
+                killer = groups[1]
+                killer_id = int(groups[2], 16)
+                killer_profile = STEAM_PROFILE_URL.format(id=killer_id)
+                killed = groups[4]
+                killed_id = int(groups[5], 16)
+                killed_profile = STEAM_PROFILE_URL.format(id=killed_id)
+                damage_type = groups[6]
+
+                action = groups[3]
+                if killed_id == killer_id:
+                    action = "suicide"
+                    damage_type = damage_type.replace("SUICIDE_", "")
+
+                action_formatted = {
+                    "killed": "Kill",
+                    "teamkilled": "Team Kill",
+                    "suicide": "Suicide",
+                }[action]
+
+                color = {
+                    "killed": 3066993,
+                    "teamkilled": 15158332,
+                    "suicide": 9807270,
+                }[action]
+
+                embed = discord.Embed(
+                    title=action_formatted,
+                    timestamp=isodate,
+                    color=color,
+                ).add_field(
+                    name="Killer",
+                    value=killer,
+                    inline=True,
+                ).add_field(
+                    name="Victim",
+                    value=killed,
+                    inline=True,
+                ).add_field(
+                    name=discord.Embed.Empty,
+                    value=discord.Embed.Empty,
+                    inline=False,
+                ).add_field(
+                    name="Killer ID",
+                    value=f"[{killer_id}]({killer_profile})",
+                    inline=True,
+                ).add_field(
+                    name="Victim ID",
+                    value=f"[{killed_id}]({killed_profile})",
+                    inline=True,
+                ).add_field(
+                    name="Damage Type",
+                    value=damage_type,
+                )
+
+        except Exception as e:
+            logger.error("error creating embed message: {e}",
+                         e=e, exc_info=True)
+
         webhook_id = self.server.discord_config["ident"][0]
         webhook_token = self.server.discord_config["ident"][1]
         webhook = Webhook.partial(
             id=webhook_id, token=webhook_token, adapter=RequestsWebhookAdapter()
         )
-        webhook.send(msg)
+
+        if embed is not None:
+            logger.info("sending webhook message for {i}", i=ident)
+            webhook.send(embed=embed)
+        else:
+            logger.info("sending webhook embed for {i}", i=ident)
+            webhook.send(content=msg)
 
     def handle(self):
         try:
@@ -146,6 +230,7 @@ def main():
         server = TKLServer(addr, TKLRequestHandler, stop_event=stop_event,
                            discord_config=config["discord"])
         logger.info("serving at: {host}:{port}", host=addr[0], port=addr[1])
+        logger.info("press CTRL+C to shut down the server")
         server.serve_forever()
     except KeyboardInterrupt:
         logger.info("server stop requested")
